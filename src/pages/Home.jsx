109065cf8bc444d909d81726075bdd0e6ba2d42b
Arkopravo <arkopravoghosh789@gmail.com>
2025-10-09 18:01:49 +0530

    complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
 import AboutSection from '../components/AboutSection'
 import SkillsSection from '../components/SkillsSection'
+import ProjectsSection from '../components/ProjectsSection'
+import ContactSection from '../components/ContactSection'
+import { Footer } from '../components/Footer'
 
 const Home = () => {
   return (
@@ -23,9 +26,12 @@ const Home = () => {
           <HeroSection/>
           <AboutSection/>
           <SkillsSection/>
+          <ProjectsSection/>
+          <ContactSection/>
         </main>
 
         {/* Footer */}
+        <Footer/>
     </div>
   )
 }
